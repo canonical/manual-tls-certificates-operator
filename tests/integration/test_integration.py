471ffbb03ee8f327cf73b01ecd07602cb3982157
guillaume <guillaume.belanger27@gmail.com>
2022-09-07 17:16:09 -0400

    Fixes integration tests to not use any resource when deploying the operator.

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -3,14 +3,11 @@
 
 import base64
 import logging
-from pathlib import Path
 
 import pytest
-import yaml
 from juju.errors import JujuError  # type: ignore[import]
 
 logger = logging.getLogger(__name__)
-METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 
 APPLICATION_NAME = "tls-certificates-operator"
 
@@ -41,12 +38,8 @@ class TestTLSCertificatesOperator:
     async def test_given_no_config_when_deploy_then_status_is_blocked(  # noqa: E501
         self, ops_test, charm, cleanup
     ):
-        resources = {
-            "placeholder-image": METADATA["resources"]["placeholder-image"]["upstream-source"],
-        }
         await ops_test.model.deploy(
             entity_url=charm,
-            resources=resources,
             application_name=APPLICATION_NAME,
         )
 
@@ -55,33 +48,6 @@ class TestTLSCertificatesOperator:
     async def test_given_correct_config_when_deploy_then_status_is_active(  # noqa: E501
         self, ops_test, charm, cleanup
     ):
-        resources = {
-            "placeholder-image": METADATA["resources"]["placeholder-image"]["upstream-source"],
-        }
-        certificate = self.get_certificate_from_file(filename="tests/certificate.pem")
-        ca_certificate = self.get_certificate_from_file(filename="tests/ca_certificate.pem")
-        ca_chain = self.get_certificate_from_file(filename="tests/ca_chain.pem")
-        certificate_bytes = base64.b64encode(certificate.encode("utf-8"))
-        ca_certificate_bytes = base64.b64encode(ca_certificate.encode("utf-8"))
-        ca_chain_bytes = base64.b64encode(ca_chain.encode("utf-8"))
-        config = {
-            "certificate": certificate_bytes.decode("utf-8"),
-            "ca-chain": ca_chain_bytes.decode("utf-8"),
-            "ca-certificate": ca_certificate_bytes.decode("utf-8"),
-        }
-
-        await ops_test.model.deploy(
-            entity_url=charm, resources=resources, application_name=APPLICATION_NAME, config=config
-        )
-
-        await ops_test.model.wait_for_idle(apps=[APPLICATION_NAME], status="active", timeout=1000)
-
-    async def test_given_correct_config_when_deploy_and_scale_then_status_is_active(  # noqa: E501
-        self, ops_test, charm, cleanup
-    ):
-        resources = {
-            "placeholder-image": METADATA["resources"]["placeholder-image"]["upstream-source"],
-        }
         certificate = self.get_certificate_from_file(filename="tests/certificate.pem")
         ca_certificate = self.get_certificate_from_file(filename="tests/ca_certificate.pem")
         ca_chain = self.get_certificate_from_file(filename="tests/ca_chain.pem")
@@ -96,7 +62,29 @@ class TestTLSCertificatesOperator:
 
         await ops_test.model.deploy(
             entity_url=charm,
-            resources=resources,
+            application_name=APPLICATION_NAME,
+            config=config,
+        )
+
+        await ops_test.model.wait_for_idle(apps=[APPLICATION_NAME], status="active", timeout=1000)
+
+    async def test_given_correct_config_when_deploy_and_scale_then_status_is_active(  # noqa: E501
+        self, ops_test, charm, cleanup
+    ):
+        certificate = self.get_certificate_from_file(filename="tests/certificate.pem")
+        ca_certificate = self.get_certificate_from_file(filename="tests/ca_certificate.pem")
+        ca_chain = self.get_certificate_from_file(filename="tests/ca_chain.pem")
+        certificate_bytes = base64.b64encode(certificate.encode("utf-8"))
+        ca_certificate_bytes = base64.b64encode(ca_certificate.encode("utf-8"))
+        ca_chain_bytes = base64.b64encode(ca_chain.encode("utf-8"))
+        config = {
+            "certificate": certificate_bytes.decode("utf-8"),
+            "ca-chain": ca_chain_bytes.decode("utf-8"),
+            "ca-certificate": ca_certificate_bytes.decode("utf-8"),
+        }
+
+        await ops_test.model.deploy(
+            entity_url=charm,
             application_name=APPLICATION_NAME,
             config=config,
         )
@@ -106,7 +94,10 @@ class TestTLSCertificatesOperator:
         await ops_test.model.applications[APPLICATION_NAME].scale(2)
 
         await ops_test.model.wait_for_idle(
-            apps=[APPLICATION_NAME], status="active", timeout=1000, wait_for_units=2
+            apps=[APPLICATION_NAME],
+            status="active",
+            timeout=1000,
+            wait_for_units=2,
         )
 
         await ops_test.model.applications[APPLICATION_NAME].scale(1)
