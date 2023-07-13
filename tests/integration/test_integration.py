3781f259a312b8d88353b372f0ca7214b1dbc90d
yazansalti <yazan.salti@canonical.com>
2023-07-13 14:22:13 +0400

    Adds requirer charm to the integration tests Checks if certificate is provided using the get-certificate action of the tls-requirer-operator charm

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -10,6 +10,7 @@ from juju.errors import JujuError
 logger = logging.getLogger(__name__)
 
 APPLICATION_NAME = "tls-certificates-operator"
+TLS_REQUIRER_CHARM_NAME = "tls-certificates-requirer"
 
 
 class TestTLSCertificatesOperator:
@@ -106,3 +107,47 @@ class TestTLSCertificatesOperator:
         await ops_test.model.applications[APPLICATION_NAME].scale(1)
 
         await ops_test.model.wait_for_idle(apps=[APPLICATION_NAME], status="active", timeout=1000)
+
+    async def test_given_tls_requirer_is_deployed_and_related_then_certificate_is_created_and_passed_correctly(  # noqa: E501
+        self, ops_test, charm, cleanup
+    ):
+        await ops_test.model.deploy(
+            TLS_REQUIRER_CHARM_NAME,
+            application_name=TLS_REQUIRER_CHARM_NAME,
+            channel="edge",
+        )
+        await ops_test.model.add_relation(
+            relation1=f"{APPLICATION_NAME}:certificates", relation2=f"{TLS_REQUIRER_CHARM_NAME}"
+        )
+        await ops_test.model.wait_for_idle(
+            apps=[TLS_REQUIRER_CHARM_NAME],
+            status="active",
+            timeout=1000,
+        )
+        action_output = await run_get_certificate_action(ops_test)
+        assert action_output["certificate"] == self.get_certificate_from_file(
+            filename="tests/certificate.pem"
+        )
+        assert action_output["ca-certificate"] == self.get_certificate_from_file(
+            filename="tests/ca_certificate.pem"
+        )
+        assert action_output["chain"] == self.get_certificate_from_file(
+            filename="tests/ca_chain.pem"
+        )
+
+
+async def run_get_certificate_action(ops_test) -> dict:
+    """Runs `get-certificate` on the `tls-requirer-requirer/0` unit.
+
+    Args:
+        ops_test (OpsTest): OpsTest
+
+    Returns:
+        dict: Action output
+    """
+    tls_requirer_unit = ops_test.model.units[f"{TLS_REQUIRER_CHARM_NAME}/0"]
+    action = await tls_requirer_unit.run_action(
+        action_name="get-certificate",
+    )
+    action_output = await ops_test.model.get_action_output(action_uuid=action.entity_id, wait=240)
+    return action_output
