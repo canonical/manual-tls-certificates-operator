d23ff7580e92b52cfaf9b9f822f67b8608690653
Patricia Reinoso <patricia.reinoso@canonical.com>
2023-09-26 13:04:29 +0000

    Add Integration tests
    Signed-off-by: Patricia Reinoso <patricia.reinoso@canonical.com>

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -3,9 +3,15 @@
 
 import ast
 import base64
+import datetime
 import logging
 
 import pytest
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.x509.oid import NameOID
 from juju.errors import JujuError
 
 logger = logging.getLogger(__name__)
@@ -27,6 +33,51 @@ class TestTLSCertificatesOperator:
         with open(filename, "r") as file:
             certificate = file.read()
         return certificate
+
+    @staticmethod
+    def get_certificate_from_csr(csr: str) -> {}:
+        csr_bytes = csr.encode("utf-8")
+        csr = x509.load_pem_x509_csr(csr_bytes, default_backend())
+
+        ca_private_key = rsa.generate_private_key(
+            public_exponent=65537, key_size=2048, backend=default_backend()
+        )
+
+        ca_name = x509.Name(
+            [
+                x509.NameAttribute(NameOID.COMMON_NAME, "Example CA"),
+            ]
+        )
+
+        ca_subject = issuer = ca_name
+        ca_cert = (
+            x509.CertificateBuilder()
+            .subject_name(ca_subject)
+            .issuer_name(issuer)
+            .public_key(ca_private_key.public_key())
+            .serial_number(x509.random_serial_number())
+            .not_valid_before(datetime.datetime.utcnow())
+            .not_valid_after(datetime.datetime.utcnow() + datetime.timedelta(days=365))
+            .add_extension(
+                x509.BasicConstraints(ca=True, path_length=0),
+                critical=True,
+            )
+            .sign(ca_private_key, hashes.SHA256(), default_backend())
+        )
+        cert = (
+            x509.CertificateBuilder()
+            .subject_name(csr.subject)
+            .issuer_name(ca_subject)
+            .public_key(csr.public_key())
+            .serial_number(x509.random_serial_number())
+            .not_valid_before(datetime.datetime.utcnow())
+            .not_valid_after(datetime.datetime.utcnow() + datetime.timedelta(days=365))
+            .sign(ca_private_key, hashes.SHA256(), default_backend())
+        )
+        return {
+            "certificate": cert,
+            "ca_cert": ca_cert,
+        }
 
     @pytest.fixture()
     async def cleanup(self, ops_test):
@@ -109,11 +160,7 @@ class TestTLSCertificatesOperator:
             timeout=1000,
         )
 
-        certificate = self.get_certificate_from_file(filename="tests/certificate.pem")
-        ca_certificate = self.get_certificate_from_file(filename="tests/ca_certificate.pem")
         ca_chain = self.get_certificate_from_file(filename="tests/ca_chain.pem")
-        certificate_bytes = base64.b64encode(certificate.encode("utf-8"))
-        ca_certificate_bytes = base64.b64encode(ca_certificate.encode("utf-8"))
         ca_chain_bytes = base64.b64encode(ca_chain.encode("utf-8"))
 
         action_output = await run_get_outstanding_csrs_action(ops_test)
@@ -122,7 +169,13 @@ class TestTLSCertificatesOperator:
         csr = action_result_list[0]["unit_csrs"][0]["certificate_signing_request"]
         csr_bytes = base64.b64encode(csr.encode("utf-8"))
 
-        await run_provide_certificate_action(
+        certs = self.get_certificate_from_csr(csr)
+        certificate_pem = certs["certificate"].public_bytes(serialization.Encoding.PEM)
+        ca_certificate_pem = certs["ca_cert"].public_bytes(serialization.Encoding.PEM)
+        certificate_bytes = base64.b64encode(certificate_pem)
+        ca_certificate_bytes = base64.b64encode(ca_certificate_pem)
+
+        action_output = await run_provide_certificate_action(
             ops_test,
             relation_id=relation.id,
             certificate=certificate_bytes.decode("utf-8"),
@@ -138,8 +191,9 @@ class TestTLSCertificatesOperator:
         )
 
         action_output = await run_get_certificate_action(ops_test)
-        assert action_output["certificate"] == certificate.strip("\n")
-        assert action_output["ca-certificate"] == ca_certificate.strip("\n")
+
+        assert action_output["certificate"] == certificate_pem.decode("utf-8").strip("\n")
+        assert action_output["ca-certificate"] == ca_certificate_pem.decode("utf-8").strip("\n")
         formatted_chain = (
             action_output["chain"]
             .replace("[", "")
